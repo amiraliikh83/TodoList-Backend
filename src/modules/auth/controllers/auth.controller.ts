aba8a6534a301e011471fb82b3bba802ea00eb87
amiraliikh83 <amiraliikh83@gmail.com>
2025-03-14 13:42:52 +0330

    fixed status code item

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -12,8 +12,8 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   @ApiResponse({ status: 201, description: 'User successfully registered.' })
   @ApiResponse({ status: 400, description: 'Bad Request.' })
-  async register(@Body() createUserDto: CreateUserDto): Promise<{ message: string }> {
+  async register(@Body() createUserDto: CreateUserDto): Promise<{ message: string  , statusCode : number}> {
     await this.authService.register(createUserDto);
-    return { message: 'User registered successfully.' };
+    return { message: 'User registered successfully.' , statusCode : HttpStatus.CREATED  };
   }
 }
