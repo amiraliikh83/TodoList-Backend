5735518f4540175cc3510f4bc1b66c80b2b23de9
amiraliikh83 <amiraliikh83@gmail.com>
2025-04-16 13:30:39 +0330

    add everythings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { ModuleRef } from '@nestjs/core';
+import { HandlerService } from './modules/handler/handler.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-auth'),
     AuthModule,
   ],
+  providers: [HandlerService],
 })
 export class AppModule {}
 // test
